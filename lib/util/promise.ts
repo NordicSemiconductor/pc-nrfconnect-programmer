d0434523bbec790e05b1affa33e9513c1c6b76d8
Chun Fan <4866953+chunfantasy@users.noreply.github.com>
2021-03-27 00:34:12 +0100

    Change promise.js to ts

--- lib/util/promise.ts
+++ lib/util/promise.ts
@@ -34,21 +34,26 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/* eslint-disable import/prefer-default-export */
-
 /**
  * Run promises in sequence and return the result in an array.
  *
  * @param {Function} fun Function that returns a promise.
  * @param {Array} results Initial array that will hold the results.
- * @param {Array} first First arguments in array.
- * @param {Array} rest Rest of the arguments in array.
- * @returns {Array} Promise resolved to the initial results array
+ * @param {Array} argsArray First arguments in array.
+ * @returns {Promise} Promise resolved to the initial results array
  * appended with the result of each promise.
  */
-export const sequence = (fun, results = [], first, ...rest) =>
-    first
+const sequence = (
+    fun: (...args: any) => Promise<unknown>,
+    results: Array<unknown> = [],
+    argsArray: Array<Array<unknown>>
+): Promise<unknown> => {
+    const [first, ...rest] = argsArray;
+    return first
         ? fun(...first).then(result =>
-              sequence(fun, [...results, result], ...rest)
+              sequence(fun, [...results, result], rest)
           )
         : Promise.resolve(results);
+};
+
+export default sequence;
