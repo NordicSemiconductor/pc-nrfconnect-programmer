7d00808c239e818d805b128a5d99e1b927409ead
Chun Fan <4866953+chunfantasy@users.noreply.github.com>
2021-04-13 15:06:48 +0200

    Update memory map types

--- types/nrf-intel-hex/index.d.ts
+++ types/nrf-intel-hex/index.d.ts
@@ -1,49 +1,49 @@
 /**
  * Parser/writer for the "Intel hex" format.
  */
-declare module 'nrf-intel-hex' {
-  export type MemoryBlocks =
-    | { [x: number]: Uint8Array }
-    | [number, Uint8Array][];
+declare module "nrf-intel-hex" {
+    export type MemoryBlocks =
+        | { [x: number]: Uint8Array }
+        | [number, Uint8Array][];
 
-  export default class MemoryMap extends Map<number, Uint8Array> {
-    constructor(blocks: MemoryBlocks);
+    export default class MemoryMap extends Map<number, Uint8Array> {
+        constructor(blocks: MemoryBlocks);
 
-    public fromHex(hexText: string, maxBlockSize: number): MemoryMap;
+        public fromHex(hexText: string, maxBlockSize: number): MemoryMap;
 
-    public join(maxBlockSize: number): MemoryMap;
+        public join(maxBlockSize: number): MemoryMap;
 
-    public overlapMemoryMaps(
-      memoryBlock: MemoryBlocks
-    ): Map<number, Uint8Array>;
+        static overlapMemoryMaps(
+            memoryBlock: MemoryBlocks
+        ): Map<number, Uint8Array>;
 
-    public flattenOverlaps(overlaps: Map<number, Uint8Array>): MemoryMap;
+        static flattenOverlaps(overlaps: Map<number, Uint8Array>): MemoryMap;
 
-    public paginate(pageSize?: number, pad?: number): MemoryMap;
+        public paginate(pageSize?: number, pad?: number): MemoryMap;
 
-    public getUint32(
-      offset: number,
-      littleEndian?: boolean
-    ): number | undefined;
+        public getUint32(
+            offset: number,
+            littleEndian?: boolean
+        ): number | undefined;
 
-    public asHexString(lineSize?: number): string;
+        public asHexString(lineSize?: number): string;
 
-    public clone(): MemoryMap;
+        public clone(): MemoryMap;
 
-    public fromPaddedUint8Array(
-      bytes: Uint8Array,
-      padByte?: number,
-      minPadLength?: number
-    ): MemoryMap;
+        public fromPaddedUint8Array(
+            bytes: Uint8Array,
+            padByte?: number,
+            minPadLength?: number
+        ): MemoryMap;
 
-    public slice(address: number, length: number): MemoryMap;
+        public slice(address: number, length: number): MemoryMap;
 
-    public slicePad(
-      address: number,
-      length: number,
-      padByte?: number
-    ): MemoryMap;
+        public slicePad(
+            address: number,
+            length: number,
+            padByte?: number
+        ): MemoryMap;
 
-    public contains(memMap: MemoryMap): boolean;
-  }
+        public contains(memMap: MemoryMap): boolean;
+    }
 }
