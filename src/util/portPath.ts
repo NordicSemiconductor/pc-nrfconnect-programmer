09fe30aad36f729c10a759a54907e4c1c97f944b
Nicolai Berthelsen <nicolai.berthelsen@nordicsemi.no>
2021-10-21 15:30:16 +0200

    Use proper SerialPort type instead of any

--- src/util/portPath.ts
+++ src/util/portPath.ts
@@ -4,14 +4,15 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
+import { SerialPort } from '@nordicsemiconductor/nrf-device-lib-js';
+
 /**
- * Prefer to use the serialport 8 property or fall back to the serialport 7 property.
  *
- * @param {any} serialPort serialport v8/v7
+ * @param {SerialPort} serialPort
  *
  * @returns {string} the path of serialport
  * appended with the result of each promise.
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default (serialPort: any): string =>
-    serialPort ? serialPort.comName : undefined;
+
+export default (serialPort: SerialPort): string | null =>
+    serialPort?.comName || null;
