4d7f9c15b683f266873fa4728f785604e9b69a1c
Dee Roberts <dee.roberts@nordicsemi.no>
2022-03-17 15:36:39 +0100

    Parse arguments

--- src/components/AppMainView.tsx
+++ src/components/AppMainView.tsx
@@ -9,6 +9,7 @@ import { useSelector } from 'react-redux';
 
 import { getLoaded, getZipFilePath } from '../reducers/fileReducer';
 import { getDeviceInfo, getSerialNumber } from '../reducers/targetReducer';
+import useArgs from '../useArgs';
 import { DeviceDefinition } from '../util/devices';
 import McuUpdateDialogView from './McuUpdateDialogView';
 import MemoryBoxView from './MemoryBoxView';
@@ -37,6 +38,8 @@ const AppMainView = () => {
     const zipFilePath = useSelector(getZipFilePath);
     const serialNumber = useSelector(getSerialNumber);
     const deviceInfo = useSelector(getDeviceInfo);
+
+    useArgs();
 
     return (
         <div className="app-main-view">
---
+++ src/useArgs.ts
@@ -0,0 +1,86 @@
+import { logger } from 'pc-nrfconnect-shared';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as fs from 'fs';
+
+import * as fileActions from './actions/fileActions';
+import * as targetActions from './actions/targetActions';
+
+import { getAreArgsParsed, argsParsed } from './reducers/argsReducer';
+import { getConnectedDevices } from './reducers/targetReducer';
+import { RootState, TDispatch } from './reducers/types';
+
+interface Args {
+    deviceSerial?: string;
+    filePath?: string;
+}
+
+/**
+ * When used, parses command line arguments passed to the Programmer
+ * and performs any effects related to those.
+ *
+ * Current effects are:
+ *
+ * `--deviceSerial` - Opens the device with the given serial number.
+ *
+ * `--filePath` - Adds the file at the given path.
+ *
+ */
+export default function useArgs(): void {
+    const dispatch = useDispatch();
+
+    const connectedDevices = useSelector(getConnectedDevices);
+    const areArgsParsed = useSelector(getAreArgsParsed);
+
+    useEffect(() => {
+        if (!areArgsParsed && Object.keys(connectedDevices).length) {
+            const args = parseArgs(process.argv);
+            dispatch(actOnArgs(args));
+        }
+    }, [connectedDevices]);
+}
+
+function parseArgs(argv: string[]): Args {
+    const serialIndex = argv.findIndex(arg => arg === '--deviceSerial');
+    const deviceSerial = serialIndex > -1 ? argv[serialIndex + 1] : undefined;
+
+    const fileIndex = argv.findIndex(arg => arg === '--filePath');
+    const filePath = fileIndex > -1 ? argv[fileIndex + 1] : undefined;
+
+    return { deviceSerial, filePath };
+}
+
+function actOnArgs({ filePath, deviceSerial }: Args) {
+    return function (dispatch: TDispatch, getState: () => RootState) {
+        if (filePath) {
+            if (fs.existsSync(filePath)) {
+                dispatch(fileActions.openFile(filePath));
+                logger.info(
+                    `Opening device ${deviceSerial} (passed as argument to --filePath)`
+                );
+            } else {
+                logger.warn(
+                    `No file found at ${filePath} (passed as argument to --filePath)`
+                );
+            }
+        }
+
+        if (deviceSerial) {
+            const device = getState().device.devices[deviceSerial];
+            if (device) {
+                // Select it in the DeviceSelector component.
+                dispatch({ type: 'device/selectDevice', payload: device });
+                dispatch(targetActions.openDevice(device));
+                logger.info(
+                    `Opening device ${deviceSerial} (passed as argument to --deviceSerial)`
+                );
+            } else {
+                logger.warn(
+                    `No device with serial number ${deviceSerial} found (passed as argument to --deviceSerial)`
+                );
+            }
+        }
+
+        dispatch(argsParsed());
+    };
+}
