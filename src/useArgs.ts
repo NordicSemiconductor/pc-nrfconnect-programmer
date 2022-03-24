88dc905e35c8b50b011f66010131276872825b3d
Marko Schulz <Marko.Schulz@nordicsemi.no>
2022-03-24 15:23:37 +0100

    Simplify
    After moving the functionality to select a device, the code to select
    the file through an argument could be simplified.

--- src/useArgs.ts
+++ src/useArgs.ts
@@ -12,49 +12,30 @@ import { logger } from 'pc-nrfconnect-shared';
 import * as fileActions from './actions/fileActions';
 import { TDispatch } from './reducers/types';
 
-interface Args {
-    filePath?: string;
-}
+const openFileFromArgs = (dispatch: TDispatch) => {
+    const { argv } = process;
 
-/**
- * When used, parses command line arguments passed to the Programmer
- * and performs any effects related to those.
- *
- * Current effects are:
- *
- * `--filePath` - Adds the file at the given path.
- *
- * @returns {void}
- */
-export default function useArgs(): void {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        const args = parseArgs(process.argv);
-        dispatch(actOnArgs(args));
-    }, [dispatch]);
-}
-
-function parseArgs(argv: string[]): Args {
     const fileIndex = argv.findIndex(arg => arg === '--filePath');
     const filePath = fileIndex > -1 ? argv[fileIndex + 1] : undefined;
 
-    return { filePath };
-}
-
-function actOnArgs({ filePath }: Args) {
-    return (dispatch: TDispatch) => {
-        if (filePath) {
-            if (fs.existsSync(filePath)) {
-                dispatch(fileActions.openFile(filePath));
-                logger.info(
-                    `Opening file ${filePath} (passed as argument to --filePath)`
-                );
-            } else {
-                logger.warn(
-                    `No file found at ${filePath} (passed as argument to --filePath)`
-                );
-            }
+    if (filePath) {
+        if (fs.existsSync(filePath)) {
+            dispatch(fileActions.openFile(filePath));
+            logger.info(
+                `Opening file ${filePath} (passed as argument to --filePath)`
+            );
+        } else {
+            logger.warn(
+                `No file found at ${filePath} (passed as argument to --filePath)`
+            );
         }
-    };
-}
+    }
+};
+
+export default () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        openFileFromArgs(dispatch);
+    }, [dispatch]);
+};
