3d2a96e93cc10b1119d96f80c7f17864993ce99c
Svyatoslav <kiev.devel@gmail.com>
2023-03-08 14:29:03 +0100

    Add licences

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
 export const timerTimeToSeconds = (
     seconds: number,
     minutes: number,
