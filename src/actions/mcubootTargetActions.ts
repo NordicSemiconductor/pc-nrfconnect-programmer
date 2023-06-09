ec4f240c03b88c7c12340cce2e26dc01b7828bdc
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-06-09 11:23:00 +0200

    Update target device after waiting for device

--- src/actions/mcubootTargetActions.ts
+++ src/actions/mcubootTargetActions.ts
@@ -28,6 +28,7 @@ import {
 import { modemKnown } from '../reducers/modemReducer';
 import {
     loadingEnd,
+    targetDeviceKnown,
     targetTypeKnown,
     targetWritableKnown,
 } from '../reducers/targetReducer';
@@ -244,6 +245,7 @@ export const performUpdate =
                     timeout: 99999999999999, // Wait 'indefinitely' as we will cancel the wait when programming is complete
                     when: 'always',
                     once: false,
+                    onSuccess: d => dispatch(targetDeviceKnown(d)),
                 })
             );
             dispatch(mcubootWritingStart());
