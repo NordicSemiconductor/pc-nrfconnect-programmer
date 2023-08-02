94d4deb029e3d4c7fecced3709193c8bbaff9a40
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-08-02 12:53:49 +0200

    Fix: show correct messages

--- src/components/MemoryView.tsx
+++ src/components/MemoryView.tsx
@@ -51,9 +51,9 @@ const MemoryView = ({ isTarget }: MemoryViewProps) => {
         isTarget ? state.app.target.regions : state.app.file.regions
     );
     const zipFilePath = useSelector(getZipFilePath);
-    const isJLink = useSelector(getForceMcuBoot) || !!device?.traits.jlink;
+    const isJLink = !useSelector(getForceMcuBoot) && !!device?.traits.jlink;
     const isNordicDfu =
-        useSelector(getForceMcuBoot) || !!device?.traits.nordicDfu;
+        !useSelector(getForceMcuBoot) && !!device?.traits.nordicDfu;
     const isMcuboot = useSelector(getForceMcuBoot) || !!device?.traits.mcuBoot;
     const isWriting = useSelector(getIsWriting);
     const isErasing = useSelector(getIsErasing);
